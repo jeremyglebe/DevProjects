0bd572a933d9a1aee459892a235f03ded02c76c2
Jeremy Glebe <jeremy.glebe@christushealth.org>
2022-07-06 14:59:30 -0500

    added insult generator

--- scripts/data.js
+++ scripts/data.js
@@ -110,5 +110,14 @@ let projects =
             tags: ["Python", "Turtle", "Brython", "Art", "Animation"],
             pubLink: "https://jeremyglebe.com/TurtleArt/",
             srcLink: "https://github.com/jeremyglebe/TurtleArt"
+        },
+        {
+            title: "Insult Generator",
+            desc: "My class of 5th/6th graders was making a pretty silly insult generator project to learn about arrays. "
+                + "(Inspired by Nick Morgan’s book, “JavaScript for Kids”) I decided to make my own version. "
+                + "Then I decided to add emojis. Then cycling insults and a pause feature and - well it got out of hand. "
+                + "Enjoy!",
+            tags: ["p5.js", "Animation", "Mobile", "Javascript", "Silly"],
+            pubLink: "https://jeremyglebe.com/Random/InsultMe/"
         }
     ];
